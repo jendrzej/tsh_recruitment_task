03a7ee5ec4d54cf0758e955e2d0e956c7aa781d3
Mateusz Ziółkowski <m.ziolkowski13@gmail.com>
2020-07-28 13:06:43 +0200

    add validation to username input before request, show red border on error

--- src/app.js
+++ src/app.js
@@ -6,22 +6,37 @@ export default class App {
   initializeApp() {
     $('#button-load').on('click', (e) => {
       let userName = $('#input-username').val();
-
-      fetch('https://api.github.com/users/' + userName)
-        .then((response) => {
-          if(!response.ok) throw new Error('User not found');
-          else return response.json();
-        })
-        .then((body) => {
-          this.profile = body;
-          this.updateProfile();
-        })
-        .catch((err) => {
-          alert(err.message);
-        });
-
+      if(this.validateUsernameInput(userName)) {
+        fetch('https://api.github.com/users/' + userName)
+          .then((response) => {
+            if(!response.ok) throw new Error('User not found');
+            else return response.json();
+          })
+          .then((body) => {
+            this.profile = body;
+            this.updateProfile();
+          })
+          .catch((err) => {
+            alert(err.message);
+          });        
+      };
     })
 
+  }
+
+  validateUsernameInput(username) {
+    const input = $('#input-username');
+    if(!this.isUsernameValid(username)) {
+      input.addClass('is-danger');
+      return false;
+    }
+    input.removeClass('is-danger');
+    return true;
+  }
+
+  isUsernameValid(username) {
+    const pattern = /^[a-z0-9_-]+$/;
+    return username && pattern.test(username);
   }
 
   updateProfile() {
