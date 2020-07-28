60636381f44ee6fe9eeaa6aa039578225446b187
Mateusz Ziółkowski <m.ziolkowski13@gmail.com>
2020-07-28 11:36:56 +0200

    select elements by ids, add id to input and button

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ export default class App {
   initializeApp() {
     let self = this;
 
-    $('.load-username').on('click', function (e) {
-      let userName = $('.username.input').val();
+    $('#button-load').on('click', function (e) {
+      let userName = $('#input-username').val();
 
       fetch('https://api.github.com/users/' + userName)
         .then((response)=> {response.json})
@@ -21,7 +21,7 @@ export default class App {
   }
 
   update_profile() {
-    $('#profile-name').text($('.username.input').val())
+    $('#profile-name').text($('#input-username').val())
     $('#profile-image').attr('src', this.profile.avatar_url)
     $('#profile-url').attr('href', this.profile.html_url).text(this.profile.login)
     $('#profile-bio').text(this.profile.bio || '(no information)')
